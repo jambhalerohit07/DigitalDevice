526670e80d56a1587d94713aa408cee526ea93d8
Rohit <jambhalerohit07@gmail.com>
2024-07-25 20:37:18 +0530

    Add project files.

---
+++ DigitalDevice/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace DigitalDevice.Models
+{
+	public class Product
+	{
+		public int Id { get; set; }
+		[MaxLength(100)]
+		public string Name { get; set; } = string.Empty;
+		[MaxLength(100)]
+		public string Brand { get; set; } = string.Empty;
+		[MaxLength(100)]
+		public string Category { get; set; } = string.Empty;
+		[Precision(16, 2)]
+		public string Price { get; set; } = string.Empty;
+
+		public string Description { get; set; } = string.Empty;
+
+		[MaxLength(100)]
+		public string ImageFileName { get; set; } = string.Empty;
+
+		public DateTime CreatedAt { get; set; }
+	}
+}
---
+++ DigitalDevice/Services/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using DigitalDevice.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DigitalDevice.Services
+{
+	public class ApplicationDbContext : DbContext
+	{
+        public ApplicationDbContext(DbContextOptions options) : base(options) 
+        {
+            
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
